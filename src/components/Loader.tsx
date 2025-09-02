afca3d9328e66d8c2c71d5330968cd7d57b4914d
David Scholer <davidascholer@gmail.com>
2025-09-02 16:06:01 -0700

    Loader edit

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,7 +2,7 @@ import { LoaderOne } from "@/components/ui/loader";
 
 const Loader = ({ className }: { className?: string }) => {
   return (
-    <div role="status">
+    <div role="status" className="mx-auto">
       <LoaderOne className={className} />
       <span className="sr-only">Loading...</span>
     </div>
