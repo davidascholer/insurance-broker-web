fcf88598968fc2bae27395cc8b8858b2540e59b5
David Scholer <davidascholer@gmail.com>
2026-01-15 17:00:51 -0800

    base components created

---
+++ src/features/blog-creator/components/ContentWithHeading.tsx
@@ -0,0 +1,34 @@
+import { cn } from "@/lib/utils";
+
+const ContentWithHeading = ({
+  heading,
+  children,
+  className,
+  headerClassName,
+}: {
+  heading: string;
+  children: React.ReactNode;
+  className?: string;
+  headerClassName?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "flex flex-col items-start gap-1 tracking-tight max-w-3xl px-4 w-full justify-start xl:text-lg",
+        className
+      )}
+    >
+      <h2
+        className={cn(
+          "text-(--primary-teal-dark) text-2xl sansita-bold mb-2",
+          headerClassName
+        )}
+      >
+        {heading}
+      </h2>
+      <div className="ml-4">{children}</div>
+    </div>
+  );
+};
+
+export default ContentWithHeading;
