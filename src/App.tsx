da8d2c0a75a9da4ae2170de8bd1208d010beee01
David Scholer <davidascholer@gmail.com>
2026-02-28 13:25:08 -0800

    only track in Prod

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import { useEffect } from "react";
 import AppRouter from "./AppRouter";
 import { BrowserRouter } from "react-router";
 import { trackUTM } from "./api/api";
+import { IS_PROD } from "./lib/constants";
 // import { useContext } from "react";
 // import AppThemeContext from "./theme/AppThemeContext";
 
 function App() {
   // const {theme} = useContext(AppThemeContext);
   useEffect(() => {
+    if (!IS_PROD) {
+      console.log("Not in production, skipping UTM tracking.");
+      return;
+    }
     // Send initial pageview with UTM data
     const urlParams = new URLSearchParams(window.location.search);
     const utmOrigin = urlParams.get("utm_source") || urlParams.get("utm");
